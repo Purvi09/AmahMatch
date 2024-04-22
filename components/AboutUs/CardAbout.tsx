6b1c5ce271f1e390d0dffeb3c0b05d19d1bc8a24
Yash Agarwal <yash.ag@outlook.com>
2024-04-23 04:50:38 +0530

    Removed space

--- components/AboutUs/CardAbout.tsx
+++ components/AboutUs/CardAbout.tsx
@@ -7,7 +7,7 @@ interface CardAboutProps {
 
 const CardAbout: React.FC<CardAboutProps> = ({ title, content }) => {
   return (
-    <div className=" md:p-8 gap-y-6 md:border-2 border-gray-500 rounded-lg m-4 md:w-1/4">
+    <div className="md:p-8 gap-y-6 md:border-2 border-gray-500 rounded-lg m-4 md:w-1/4">
       <div className="text-center text-2xl text-center font-semibold md:w-3/4 h-16 mb-4 ml-6">{title}</div>
       <div className="text-lg font-light text-center mt-10">{content}</div>
     </div>
